1f0c4bc8db878fe71b024df61795d66b9e0ed580
chandan <chandantejaa@gmail.com>
2021-03-08 11:32:20 +0530

    added all test cases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 public class CalculatorTest {
 
     private static final double DELTA = 1e-10;
-    private Calculator calc;
+    private static Calculator calc;
    // private static  Logger logging;
     @BeforeClass
-     public void initialize() {
+     public  static void initialize() {
         calc = new Calculator();
      //   logging = LogManager.getLogger(Calculator.class);
 
@@ -30,7 +30,7 @@ public class CalculatorTest {
     {
        assertNotEquals("[Negative Case]: Factorial of a number,",24,calc.calcFactorial(5),DELTA);
         assertNotEquals("[Negative Case]: Factorial of a number,",120,calc.calcFactorial(7),DELTA);
-        assertNotEquals("[Negative Case]: Factorial of a number,",Double.NaN,calc.calcFactorial(-3),DELTA);
+        assertNotEquals("[Negative Case]: Factorial of a number,",5,calc.calcFactorial(-3),DELTA);
     }
 
     @Test
@@ -46,10 +46,43 @@ public class CalculatorTest {
     public  void  exponentiationNegative()
     {
         assertNotEquals("[Negative Case]: Power of a Number, ",31,calc.calcExp(2,5),DELTA) ;
-        assertEquals("[Negative Case]: Power of a Number, ",0.1111111122,calc.calcExp(3,-2),DELTA);
-        assertEquals("[Negative Case]: Power of a Number, ",1,calc.calcExp(0,0),DELTA);
+        assertNotEquals("[Negative Case]: Power of a Number, ",0.1111111122,calc.calcExp(3,-2),DELTA);
+        assertNotEquals("[Negative Case]: Power of a Number, ",1,calc.calcExp(0,0),DELTA);
     }
 
+    @Test
+    public  void  logPositive()
+    {
+        assertEquals("[Positive Case]: Log of a number, ",2.302585092994,calc.calcLog(10),DELTA);
+        assertEquals("[Positive Case]: Log of a number, ",Double.NEGATIVE_INFINITY,calc.calcLog(0),DELTA);
+        assertEquals("[Positive Case]: Log of a number, ",Double.NaN,calc.calcLog(-5),DELTA);
+    }
+
+
+    @Test
+    public  void  logNegative()
+    {
+        assertNotEquals("[Negative Case]: Log of a number, ", 6, calc.calcLog(3), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", -7.3, calc.calcLog(1.6), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", 0, calc.calcLog(0), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", 9.0, calc.calcLog(-5), DELTA);
+    }
+
+
+    @Test
+    public void sqrtPositive(){
+        assertEquals("[Positive Case]: SQRT of a number, ", 2, calc.calcSQRT(4), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", 2.236067977499, calc.calcSQRT(5), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", 0, calc.calcSQRT(0), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", Double.NaN, calc.calcSQRT(-2), DELTA);
+    }
+
+    @Test
+    public void sqrtNegative(){
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 6, calc.calcSQRT(4), DELTA);
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 4.2, calc.calcSQRT(2.1), DELTA);
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 7.3, calc.calcSQRT(-5), DELTA);
+    }
 
 
 }
