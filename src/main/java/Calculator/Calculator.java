657c522aa3f5a7b1bd39fdd99173ba66b6ea4907
chandan <chandantejaa@gmail.com>
2021-03-10 19:43:35 +0530

    added docker deployment for ansible

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -4,7 +4,6 @@ package Calculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import  java.util.InputMismatchException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
